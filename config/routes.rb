1661e43475af198650c9905a00caab9732a3bcf1
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-15 09:07:00 +0000

    [add]about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/about' => "homes#about", as: 'about'
+  devise_for :users
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
